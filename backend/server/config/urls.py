b02d8b8cda42924ecf752fa0935a3322f7bb0f86
mateuszkocik <mateusz.kocik@infermedica.com>
2025-12-05 16:57:10 +0100

    feature: Add swagger

--- backend/server/config/urls.py
+++ backend/server/config/urls.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+from config.routers import DefaultExtendableRouter
+from config.generators import serve_schema_file
 from api.views import HelloApi
+
+router = DefaultExtendableRouter()
+
+apiUrls = [
+    path("", include(router.urls)),
+    path(
+        "open-api.yaml",
+        serve_schema_file,
+        name="open-api",
+    ),
+    path(
+        "docs/",
+        RedirectView.as_view(url="/api/", permanent=True),
+        name="swagger-ui-redirect",
+    ),
+    path("hello/", HelloApi.as_view()),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("hello/", HelloApi.as_view()),
+    path("api/", include((apiUrls, "api"), namespace="api")),
 ]
